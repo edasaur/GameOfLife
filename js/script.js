bdea4ddbfcd4f0adbc0b3cfb0b1c4072e0872b29
edasaur <edx.wang@gmail.com>
2014-12-27 13:16:23 -0800

    Encountering null reference. Will debug at a later time

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var canvas = document.getElementById("map");
+/*for (var x = 0.5; x < canvas.width; x += 2) {
+	context.moveTo(x,0);
+	context.lineTo(x,canvas.height);
+}
+
+for (var y = 0.5; y < canvas.height; y+=2) {
+	context.moveTo(0,y);
+	context.lineTo(canvas.width, y)
+}*/
