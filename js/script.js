7e9dce17006ad9845efe2561fecc72832b3af54e
edasaur <edx.wang@gmail.com>
2014-12-28 16:56:04 -0800

    Got selection to work. Now to get lines to work (User should be able to draw across the grid

--- js/script.js
+++ js/script.js
@@ -16,3 +16,56 @@ for (var y = 0.5; y < canvas.height; y+=5) {
 context.lineWidth = 0.5;
 context.strokeStyle = "#696969";
 context.stroke();
+
+var clickX = new Array();
+var clickY = new Array();
+var clickDrag = new Array();
+var paint;
+
+function addClick(x, y, dragging) {
+	clickX.push(x);
+	clickY.push(y);
+	clickDrag.push(dragging);
+}
+
+function redraw() {
+	//context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+	context.strokeStyle = "green";
+	context.lineJoin = "round";
+	context.lineWidth = 5;
+	
+	for (var i = 0; i < clickX.length; i++) {
+		context.beginPath();
+		if(clickDrag[i] && i) {
+			context.moveTo(clickX[i-1], clickY[i-1]);
+		} else {
+			context.moveTo(clickX[i]-1, clickY[i]);
+		}
+	context.lineTo(clickX[i], clickY[i]);
+	context.closePath();
+	context.stroke();
+	}
+}
+
+$('#map').mousedown(function(m){
+	fill = true;
+	addClick(m.pageX, m.pageY);
+	redraw();
+});
+
+$('#map').mousemove(function(m){
+	if (paint) {
+		addClick(e.pageX, e.pageY, true);
+		redraw();
+	}
+});
+
+$('#map').mouseup(function(e) {
+	paint = false;
+});
+
+$('#map').mouseleave(function(e) {
+	paint = false;
+});
+
+
